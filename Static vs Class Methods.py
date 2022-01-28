f3b70d7ad3f3099117dfa3978a78fa142083e2d6
Lucas Eduardo dos Santos de Souza <84149268+LucasEduardoSS@users.noreply.github.com>
2022-01-28 11:06:21 -0300

    Add files via upload

--- Static vs Class Methods.py
+++ Static vs Class Methods.py
@@ -2,7 +2,18 @@ class ExClass:
     @staticmethod
     def ex_static_function(num):
         pass
+        '''
+        This should do something that has a relationship
+        with the class, but not something that must be unique
+        per instance!
+        '''
 
     @classmethod
     def ex_class_function(cls):
         pass
+        '''
+        This should also do something that has a relationship
+        with the class, but usualy, those are used to
+        manipulate different structures of data to instantiate
+        objects, like we have done with CSV.
+        '''
